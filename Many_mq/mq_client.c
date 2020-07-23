a260280ce7298a722056e844e221e752af016e6d
aqaqsubin <aqaq925120k@naver.com>
2020-07-23 14:32:48 +0900

    Create Numerous Message Queue (Occur Permission Denied)

--- Many_mq/mq_client.c
+++ Many_mq/mq_client.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
 #include <mqueue.h>
-#include <sys/mman.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <pthread.h>
+#include <string.h>
+#include <unistd.h>
 
 const char* input = "input.db";
+const char * slash = "/home/root/vscode/make_mq/";
+
 int* ptr_input;
 
 void client(void * params);
+int getDigit (int pid);
 
 struct Paramter
 {
-    int server_pid;
+    int server_mqID;
 };
 
 int main(int argc, char **argv) {
@@ -24,54 +28,91 @@ int main(int argc, char **argv) {
     int status;
     int fd_input;
 
+    //Shared File : Get Message Queue ID
     if((fd_input = open(input,O_CREAT|O_RDWR,S_IRUSR|S_IWUSR))==-1){
 		perror("File Open Error\n");
 		return 1;
 	}
     ptr_input = mmap(NULL, 200*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd_input,0);
 
-    // create thread
+    //Create 200 Thread
 	for (i=0; i<200; i++){
         struct Paramter *params;
         params = (struct Paramter *)malloc(sizeof(struct Paramter));
-        params->server_pid = ptr_input[i];
+        params->server_mqID = ptr_input[i];
         
         pthread_create(&pt[i], NULL, (void*)&client, (void *)params);
     }
+    //Start 200 Thread
     for (i=0; i<200; i++){
         pthread_join(pt[i], (void*)&status);
     }
+    
     close(fd_input);
+
     return 0;
 }
 
 void client(void * params) {
     
     struct mq_attr attr;
-    attr.mq_maxmsg = 10;
+    attr.mq_maxmsg = 100;
     attr.mq_msgsize = 4;
     int value = 0;
 
+    char * mq_name;
+    char *pid_to_str;
+    int server_mqID;
+
     mqd_t mfd;
 
+    //Get Message Queue ID
     struct Paramter *my_params = (struct Paramter *)params;
-    char mq_name[10];
-    sprintf(mq_name, "%d", ptr_input[my_params->server_pid]);
+    server_mqID = ptr_input[my_params->server_mqID];
+
+    printf("Message Queue ID : %d \n",server_mqID);
+
+    //Message Queue name it Server PID + Thread_Num
+    pid_to_str = (char *)malloc(sizeof(char)*getDigit(server_mqID));
+    mq_name = (char *)calloc(0, sizeof(pid_to_str) + strlen(slash));
+    
+    sprintf(pid_to_str, "%d", server_mqID);
+
+    strncat(mq_name, slash, strlen(slash));
+    strncat(mq_name, pid_to_str, sizeof(pid_to_str));
+
+    printf("[%s] Message Queue Open \n", mq_name);
+    //Create Message Queue
     mfd = mq_open((const char *)mq_name, O_WRONLY, 0666, &attr);
     if (mfd == -1)
     { 
-            perror("MQ Open Error");
+            perror(mq_name);
             exit(0);
     } 
 
-    while(1) {
+    //100 cycle loop
+    while(value < 100) {
         if((mq_send(mfd, (char *)&value, attr.mq_msgsize, 1)) == -1){
             perror("Send Error");
             exit(-1);
         }
         sleep(1);
         value ++;
-        // printf("Send Data %d\n", value);
     }
+    printf("%d Message Queue Done !", my_params->server_mqID);
+
+    free(mq_name);
+    free(pid_to_str);
+    
     return;
+}
+
+//Return Number of Digit
+int getDigit (int pid){
+    int i =0;
+    while(pid > 0){
+        pid = (int)(pid / 10);
+        i ++;
+    }
+    return i;
 }
--- Many_mq/mq_server.c
+++ Many_mq/mq_server.c
@@ -5,11 +5,17 @@
 #include <sys/stat.h>
 #include <mqueue.h>
 #include <pthread.h>
+#include <string.h>
+#include <unistd.h>
 
 const char* input = "input.db";
+const char * slash = "/home/root/vscode/make_mq/";
+
 int* ptr_input;
+int server_pid;
 
 void server(void *params);
+int getDigit (int pid);
 
 struct Paramter
 {
@@ -17,65 +23,116 @@ struct Paramter
 };
 
 int main(int argc, char **argv) {
+
     pthread_t pt[200];
     int  i;
     int status;
     int fd_input;
-    struct Paramter *parms;
+
+    //Shared File : Get Server PID
     if((fd_input = open(input,O_CREAT|O_RDWR,S_IRUSR|S_IWUSR))==-1){
 		perror("File Open Error\n");
 		return 1;
 	}
+    server_pid = getpid();
+
+    //INIT File
     write(fd_input, "", sizeof(int)*200);
     ptr_input = mmap(NULL, 200*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd_input,0);
 
-    // create thread
+    //Create 200 Thread
 	for (i=0; i<200; i++){
+
+        //Pass Server Thread Number
         struct Paramter *params;
         params = (struct Paramter *)malloc(sizeof(struct Paramter));
         params->thread_num = i;
 
         pthread_create(&pt[i], NULL, (void*)&server, (void *)params);
     }
+
+    //Start 200 Thread
     for (i=0; i<200; i++){
         pthread_join(pt[i], (void*)&status);
     }
+
     close(fd_input);
+
     return 0;
 }
 
 void server(void *params){
 
     struct mq_attr attr;
-    attr.mq_maxmsg = 10;
+    attr.mq_maxmsg = 100;
     attr.mq_msgsize = 4;
     int value;
+
+    char *mq_name;
+    char *mq_id;
+
+    char *pid_to_str;
+    char *thrNum_to_str;
     
     mqd_t mfd;
 
+    //Get Server Thread Number
     struct Paramter *my_params = (struct Paramter *)params;
-    ptr_input[my_params->thread_num] = getpid();
+    
+    //Message Queue name it Server PID + Thread Num
 
-    char *mq_name;
-    mq_name = (char *)malloc(sizeof(char)*getDigit(getpid()));
-    sprintf(mq_name, "%d", ptr_input[my_params->thread_num]);
+    pid_to_str = (char *)malloc(sizeof(server_pid)/sizeof(int));
+    thrNum_to_str = (char *)malloc(getDigit(my_params->thread_num));
 
+    mq_name = (char *)malloc(sizeof(server_pid)/sizeof(int) + getDigit(my_params->thread_num) + strlen(slash));
+    mq_id = (char *)malloc(sizeof(server_pid)/sizeof(int) + getDigit(my_params->thread_num));
+    
+    printf("Size of slash : %d\n",sizeof(char *));
+    sprintf(pid_to_str, "%d", server_pid);
+    sprintf(thrNum_to_str, "%d", my_params->thread_num);
+
+    //Get Message Queue Name ex) '/dev/mqueue/' + '1525' + '100' = '/dev/mqueue/1525100'
+    strncat(mq_name, slash, strlen(slash));
+    strncat(mq_name, pid_to_str, sizeof(pid_to_str));
+    strncat(mq_name, thrNum_to_str, sizeof(thrNum_to_str));
+
+    //Get Message Queue ID ex) '1525' + '100' = '1525100'
+    strncat(mq_id, pid_to_str, sizeof(pid_to_str));
+    strncat(mq_id, thrNum_to_str, sizeof(thrNum_to_str));
+
+    //Write Message Queue ID
+    ptr_input[my_params->thread_num] = atoi(mq_id);
+
+    printf("[%s] Message Queue ID : %d \n", mq_id, atoi(mq_id));
+    printf("[%s] Message Queue Open \n", mq_name);
+    //Create Message Queue
     mfd = mq_open((const char *)mq_name, O_RDWR | O_CREAT, 0666, &attr);
     if (mfd == -1)
     { 
-            perror("MQ Open Error");
+            perror("Message Queue Open Error");
             exit(0);
-    } 
+    }
 
-    while(1) {
+    //100 cycle loop
+    while(value != 100) {
+        printf("[%s] Receive : %d \n", mq_id, value);
         if((mq_receive(mfd, (char *)&value, attr.mq_msgsize, NULL)) == -1){
+            perror("Send Error");
             exit(-1);
         }
-        // printf("Read Data %d\n", value);
     }
+    printf("%d Thread Done !", my_params->thread_num);
+    
+    mq_close(mfd);
+    mq_unlink((const char *)mq_name);
+    
+    free(mq_name);
+    free(pid_to_str);
+
     return;
 }
 
+//Return Number of Digit
 int getDigit(int pid){
     int i = 0;
     while(pid > 0){
