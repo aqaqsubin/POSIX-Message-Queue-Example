3aa35b7e9b886d90ac6c089891bab219f78dfa95
aqaqsubin <aqaq925120k@naver.com>
2020-07-23 10:46:24 +0900

    Complete : Message Queue name it Server PID

--- Share_text/mq_client.c
+++ Share_text/mq_client.c
@@ -6,71 +6,82 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <semaphore.h>
-
 #include <sys/types.h>
 #include <string.h>
 
 const char* input = "input.db";
+const char * slash = "/";
+
+char * mq_name;
+int * ptr_input;
 
 int server_pid;
-int* ptr_input;
 
-sem_t* inputClient;
+int getDigit (int pid);
 
 int main(int argc, char **argv)
 {
     int fd_input;
 
     struct mq_attr attr;
-    attr.mq_maxmsg = 11;
-    attr.mq_msgsize = 12*sizeof(char);
-    char value[10];
+    attr.mq_maxmsg = 10; // Size of Message Queue
+    attr.mq_msgsize = 21 * sizeof(char); // Size of Message
+
+    char value[20];
+    char *pid_to_str;
+
     mqd_t mfd;
-
-    sem_unlink("inputClient");
-
-
-    if((inputClient = sem_open("inputClient", O_CREAT,0777,1))==NULL){
-
-		perror("Sem Open Error");
-		return 1;
-	}
-   
-
-    //Shared File : Get Client PID
+    
+    //Shared File : Get Server PID
     if((fd_input = open(input,O_CREAT|O_RDWR,S_IRUSR|S_IWUSR))==-1){
 		perror("Open Error\n");
 		return 1;
 	}
 
-    //Input Server PID
-	ptr_input = mmap(NULL,2*sizeof(int), PROT_READ|PROT_WRITE,MAP_SHARED,fd_input,0);
-    ptr_input[1] = getpid();
+	ptr_input = mmap(NULL,1*sizeof(int), PROT_READ|PROT_WRITE,MAP_SHARED,fd_input,0);
+    server_pid = ptr_input[0];
 
-    mfd = mq_open("/my_mq", O_WRONLY, 0666, &attr);
+    //Message Queue name it Server PID
+    pid_to_str = (char *)malloc(sizeof(char)*getDigit(server_pid));
+    mq_name = (char *)calloc(0, sizeof(pid_to_str)+1);    sprintf(pid_to_str, "%d", server_pid);
+
+    strncat(mq_name, slash, 1);
+    strncat(mq_name, pid_to_str, sizeof(pid_to_str));
+    
+    close(fd_input);
+
+    //Create Message Queue
+    mfd = mq_open((const char *)mq_name, O_WRONLY, 0666, &attr);
     if(mfd == -1)
     {
-        perror("open error");
+        perror("Open Error");
         exit(0);
     }
-    int err;
+    
     while(1)
     {
-        printf("Client input\n");
-        scanf("%s", &value);
-        printf("%d, %d\n",sizeof(value), attr.mq_msgsize);
-        err = mq_send(mfd, (char *)&value, sizeof(value)+1, 1);
-        if(err == -1)
+        printf("Client input : ");
+        gets(value);
+        if((mq_send(mfd, (char *)&value, sizeof(value)+1, 1)) == -1)
         {
-            perror("send error");
+            perror("Send Error");
             exit(-1);
-        }else printf("Send Message\n");
+        }
         fflush(stdin);
-        printf("Client post\n");
-        sem_post(inputClient);
 
     }
-    close(fd_input);
+
+    free(mq_name);
+    free(pid_to_str);
+    
     return 0;
+}
+
+int getDigit (int pid){
+    int i =0;
+    while(pid > 0){
+        pid = (int)(pid / 10);
+        i ++;
+    }
+    return i;
 }
--- Share_text/mq_server.c
+++ Share_text/mq_server.c
@@ -6,71 +6,80 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <semaphore.h>
-
 #include <sys/types.h>
 #include <string.h>
 
 const char* input = "input.db";
+const char * slash = "/";
 
-int client_pid;
+char * mq_name;
 int* ptr_input;
 
-sem_t* inputClient;
-
+int getDigit (int pid);
 
 int main(int argc, char **argv)
 {
     int fd_input;
 
     struct mq_attr attr;
-    attr.mq_maxmsg = 10;
-    attr.mq_msgsize = 11*sizeof(char);
-    char value[10];
+    attr.mq_maxmsg = 10; // Size of Message Queue
+    attr.mq_msgsize = 21 * sizeof(char); // Size of Message
+
+    char value[20];
+    char *pid_to_str;
 
     mqd_t mfd;
-
-    sem_unlink("inputClient");
-
-    if((inputClient = sem_open("inputClient", O_CREAT,0777,0))==NULL){
-
-		perror("Sem Open Error");
-		return 1;
-	}
     
-    //Shared File : Get Client PID
+    //Shared File : Share Server PID
     if((fd_input = open(input,O_CREAT|O_RDWR,S_IRUSR|S_IWUSR))==-1){
 		perror("Open Error\n");
 		return 1;
 	}
     
     //INIT
-    write(fd_input, "", sizeof(int)*2);
+    write(fd_input, "", sizeof(int)*1);
 
     //Input Server PID
-	ptr_input = mmap(NULL,2*sizeof(int), PROT_READ|PROT_WRITE,MAP_SHARED,fd_input,0);
+	ptr_input = mmap(NULL,1*sizeof(int), PROT_READ|PROT_WRITE,MAP_SHARED,fd_input,0);
     ptr_input[0]=getpid();
 
+    //Message Queue name it Server PID
+    pid_to_str = (char *)malloc(sizeof(char)*getDigit(getpid()));
+    mq_name = (char *)calloc(0, sizeof(pid_to_str)+1);
+    sprintf(pid_to_str, "%d", getpid());
+
+    strncat(mq_name, slash, 1);
+    strncat(mq_name, pid_to_str, sizeof(pid_to_str));
+
+    close(fd_input);
+
     //Create Message Queue 
-    mfd = mq_open("/my_mq", O_RDWR | O_CREAT,  0666, &attr);
+    mfd = mq_open((const char *)mq_name, O_RDWR | O_CREAT,  0666, &attr);
     if (mfd == -1)
     {
-            perror("open error");
+            perror("Open Error");
             exit(0);
     }
 
     while(1)
     {
-        //Wait for Client
-        printf("Wait for Client input\n");
-        sem_wait(inputClient);
-        printf("Reading Server\n");
         if((mq_receive(mfd, (char *)&value, attr.mq_msgsize,NULL)) == -1)
         {
                 exit(-1);
         }
-        printf("Read Data %d\n", value);
+        printf("Read Data : %s\n", value);
     }
-    close(fd_input);
+
+    free(mq_name);
+    free(pid_to_str);
+
     return 0;
+}
+int getDigit (int pid){
+    int i =0;
+    while(pid > 0){
+        pid = (int)(pid / 10);
+        i ++;
+    }
+    return i;
 }         
